614453c8f4144bfedad616e5e417310857e36aeb
Kaamil-Saib <kaamilsaib01@gmail.com>
2024-09-09 12:16:21 +0200

    intial commit

---
+++ src/medleySimulation/FinishCounter.java
@@ -0,0 +1,43 @@
+// Simple class to record when someone has crossed the line first and wins
+package medleySimulation;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class FinishCounter {
+	private AtomicBoolean firstAcrossLine; // flag
+	private final AtomicInteger winner; // who won
+	private AtomicInteger winningTeam; // counter for patrons who have left the club
+
+	FinishCounter() {
+		firstAcrossLine = new AtomicBoolean(true);// no-one has won at start
+		this.winner = new AtomicInteger(-1); // no-one has won at start
+		winningTeam = new AtomicInteger(-1); // no-one has won at start
+	}
+
+	// This is called by a swimmer when they touch the fnish line
+	public synchronized void finishRace(int swimmer, int team) {
+		boolean won = false;
+		if (firstAcrossLine.get()) {
+			firstAcrossLine.set(false);
+			won = true;
+		}
+		if (won) {
+			winner.set(swimmer);
+			winningTeam.set(team);
+		}
+	}
+
+	// Has race been won?
+	public boolean isRaceWon() {
+		return !(firstAcrossLine.get());
+	}
+
+	public int getWinner() {
+		return winner.get();
+	}
+
+	public int getWinningTeam() {
+		return winningTeam.get();
+	}
+}
